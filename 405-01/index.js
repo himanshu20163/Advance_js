eb7799cd1e124a7fc0e55ac17d61c39baa9a87b2
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-01-15 12:55:09 +0530

    Update index.js

--- 405-01/index.js
+++ 405-01/index.js
@@ -1,104 +1,113 @@
-const numberOfCountries = document.getElementById("numberOfCountries")
-numberOfCountries.innerText = `Total Number of countries are ${countries.length}`
-
-
-
-function startingSearchFunction() {
-    const countryCardContainer = document.getElementById("countryCardContainer")
-    countryCardContainer.innerHTML = ""
-    //const inputElement = document.getElementById("countryNameInput")
-    //inputElement.setAttribute("onkeyup", "startingSearchFunction()")
-
-    const input = document.getElementById("countryNameInput").value
-
-    const matchedCountries = []
-
-    countries.map((ele) => {
-        const hello = ele.startsWith(input.toUpperCase())
-        // console.log(hello);
-        if (input == "") {   
-            countryCardContainer.innerHTML = `
-                <div class="card">
-                    <p>please enter a word</p>
-                </div>
-                `
-        } 
-        else {
-            if (hello) {
-                matchedCountries.push(ele)
-                countryCardContainer.innerHTML += `
-                <div class="card">
-                    <p>${ele}</p>
-                </div>
-                `
-                const numberOfSearchResults = document.getElementById("numberOfSearchResults")
-                numberOfSearchResults.innerText = `Number of countries starting with ${input} are ${matchedCountries.length}`
-            }
-        }
-    })
-
-    // for(let i=0; i<countriesName.length; i++){
-    //     const hello = countriesName[i].startsWith(input.toLowerCase())
-    //     // console.log(hello);
-    //     if(hello){
-    //         matchedCountries.push(countriesName[i])
-    //         countryCardContainer.innerHTML += `
-    //         <div style="border: 2px solid red; display:flex; width:15vw; padding: 1rem; justify-content: center; align-items: center; margin:1rem" >
-    //             <p>${countriesName[i]}</p>
-    //         </div>
-    //         `
-    //     }
-    //     const numberOfSearchResults = document.getElementById("numberOfSearchResults")
-    //     numberOfSearchResults.innerText = `Number of countries starting with ${input} are ${matchedCountries.length}`
-    // }
+const heading = document.getElementById("para");
+heading.innerHTML = `currently we have ${countries_data.length} countries`;
+console.log(heading);
+const sortedData = countries_data.sort(function(a, b) {
+  return b.population - a.population
+})
+let totalPopulation = 0
+for (let i = 0; i < countries_data.length; i++) {
+  totalPopulation += countries_data[i].population
 }
+const clear = document.getElementById("show_data_container")
 
-function searchAnyWordFunction() {
-    const countryCardContainer = document.getElementById("countryCardContainer")
-    countryCardContainer.innerHTML = ""
-    // console.log(countryCardContainer);
-    const inputElement = document.getElementById("countryNameInput")
-    inputElement.setAttribute("onkeyup", "searchAnyWordFunction()")
+function population_data() {
+  clear.innerHTML = " "
+  for (let i = 0; i < 10; i++) {
+    const firstdiv = document.createElement("div");
+    firstdiv.style.display = "flex";
+    firstdiv.style.height = "fit-content";
+    firstdiv.style.alignItems = "center";
+    firstdiv.style.justifyContent = "center";
+    firstdiv.style.backgroundColor = "#f2f2f2";
 
-    const input = document.getElementById("countryNameInput").value
-    const matchedCountries = []
+    //countryname
+    const countryname = document.createElement("p");
+    countryname.style.width = "30%";
+    countryname.innerHTML = countries_data[i].name;
+    firstdiv.appendChild(countryname);
+    //countrypopulation
 
-    countriesName.map((ele) => {
-        const hello = ele.includes(input.toLowerCase())
-        // console.log(hello);
-        if (input == "") {
-            countryCardContainer.innerHTML = `
-                <div class="card">
-                    <p>please enter a word</p>
-                </div>
-                `
-        } else {
-            if (hello) {
-                matchedCountries.push(ele)
-                countryCardContainer.innerHTML += `
-            <div class="card">
-                <p>${ele}</p>
-            </div>
-            `
-                const numberOfSearchResults = document.getElementById("numberOfSearchResults")
-                numberOfSearchResults.innerText = `Number of countries starting with ${input} are ${matchedCountries.length}`
-            }
-        }
+    const pdiv = document.createElement("div");
+    const percentagediv = document.createElement("div");
+    const percent = (sortedData[i].population / totalPopulation) * 100;
+    percentagediv.style.width = `${percent}%`;
+    percentagediv.style.backgroundColor = "orange";
+    percentagediv.style.border = "none";
+    percentagediv.style.height = "38px";
+    percentagediv.style.margin = "5px";
+    percentagediv.innerHTML = `${percent.toFixed(2)}%`;
+    pdiv.appendChild(percentagediv);
+    pdiv.style.width = "30%";
+    firstdiv.appendChild(pdiv);
 
-    })
+    const poplaution_div = document.createElement("div");
+    poplaution_div.innerHTML = sortedData[i].population;
+    firstdiv.appendChild(poplaution_div);
+    const data = document.getElementById("show_data_container");
+    data.appendChild(firstdiv);
+    console.log(firstdiv);
+  }
+}
+function language_data() {
+  clear.innerHTML = " "
 
-    // for(let i=0; i<countriesName.length; i++){
-    //     const hello = countriesName[i].includes(input.toLowerCase())
-    //     // console.log(hello);
-    //     if(hello){
-    //         matchedCountries.push(countriesName[i])
-    //         countryCardContainer.innerHTML += `
-    //         <div style="border: 2px solid red; display:flex; width:15vw; padding: 1rem; justify-content: center; align-items: center; margin:1rem" >
-    //             <p>${countriesName[i]}</p>
-    //         </div>
-    //         `
-    //     }
-    //     const numberOfSearchResults = document.getElementById("numberOfSearchResults")
-    //     numberOfSearchResults.innerText = `Number of countries starting with ${input} are ${matchedCountries.length}`
-    // }
+  const languagesArray = []
+
+  for (let i = 0; i < countries_data.length; i++) {
+    languagesArray.push(countries_data[i].languages)
+  }
+
+  const flattedLanguagesArray = languagesArray.flat()
+
+  const languageCount = {}
+
+  flattedLanguagesArray.map((ele) => {
+    languageCount[ele] = (languageCount[ele] || 0) + 1
+  })
+
+  const objArr = Object.entries(languageCount)
+  console.log(objArr);
+  const sortedArray = objArr.sort((a, b) => {
+    return b[1] - a[1]
+  })
+
+
+  for (let i = 0; i < 10; i++) {
+
+    const topTenValuesFronSortedArray = sortedArray[i]
+
+    const oneDiv = document.createElement("div")
+    oneDiv.style.display = "flex"
+    oneDiv.style.height = "fit-content"
+    oneDiv.style.alignItems = "center"
+    oneDiv.style.justifyContent = "center"
+    oneDiv.style.backgroundColor = "#f2f2f2";
+
+    const countryName = document.createElement("p")
+    countryName.innerText = topTenValuesFronSortedArray[0]
+    countryName.style.width = "20%"
+    oneDiv.appendChild(countryName)
+
+    const percentDiv = document.createElement("div")
+    const percentageDiv = document.createElement("div")
+    const percentage = (topTenValuesFronSortedArray[1] / sortedArray.length) * 100
+    percentageDiv.style.width = `${percentage}%`
+    percentageDiv.style.backgroundColor = "orange"
+    percentageDiv.style.height = "fit-content"
+
+    percentageDiv.innerText = `${percentage.toFixed(2)}%`
+
+    percentDiv.appendChild(percentageDiv)
+    percentDiv.style.width = "30%"
+    oneDiv.appendChild(percentDiv)
+
+    const populationDiv = document.createElement("div")
+    populationDiv.innerText = topTenValuesFronSortedArray[1]
+    oneDiv.appendChild(populationDiv)
+
+    const graphDiv = document.getElementById("show_data_container")
+    graphDiv.appendChild(oneDiv)
+
+    console.log(topTenValuesFronSortedArray);
+  }
 }
