deefd26c7b140ab7c0f7c41a27de5a83322eb4f2
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-01-17 13:40:03 +0530

    Update index.js

--- 407-01/index.js
+++ 407-01/index.js
@@ -3,39 +3,62 @@ function searchEmoji() {
     table.innerHTML = ""
     const input = document.getElementById("input").value
     const tagsArray = []
-    emojiList.map((ele) => {
-        if (ele.tags.includes(input)) {
-            tagsArray.push(ele)
-            table.innerHTML += `
-                <tr class="tablerow">
-                    <td id="emojiIcon">${ele.emoji}</td>
-                    <td id="emojiTags">${ele.aliases}</td>
-                    <td id="emojiname">${ele.description}</td>
-                </tr>
-            `
-        }
-    })
-    console.log(tagsArray);
-}
+    if(input == ""){
+        table.innerHTML =`
+                            <tr>
+                            <td id="p">Please Enter Emoji Name Here!!!!!</td>
+                            </tr>   
+                        `  
+    }
+    else{
+        emojiList.map((ele) => {
+            if (ele.tags.includes(input)) {
+                tagsArray.push(ele)
+                table.innerHTML += `
+                    <tr class="tablerow">
+                        <td id="emojiIcon">${ele.emoji}</td>
+                        <td id="emojiTags">${ele.aliases}</td>
+                        <td id="emojiname">${ele.description}</td>
+                    </tr>
+                `
+            }
+            
+        })
+        
+    }
+    }
+   
 
 function searchEmojiKeyup() {
     const table = document.getElementById("tableData")
     table.innerHTML = ""
     const input = document.getElementById("input").value
     const tagsArray = []
-    emojiList.map((ele) => {
-        ele.tags.map((element) => {
-            if (element.startsWith(input)) {
-                tagsArray.push(ele)
-                table.innerHTML += `
-                <tr class="tablerow">
-                    <td id="emojiIcon">${ele.emoji}</td>
-                    <td id="emojiTags">${ele.aliases}</td>
-                    <td id="emojiname">${ele.description}</td>
-                </tr>
-            `
-            }
+    if(input == ""){
+        table.innerHTML =`
+                            <tr>
+                            <td id="p">Please Enter Emoji Name Here!!!!!</td>
+                            </tr>   
+                        `  
+    }
+    else{
+        emojiList.map((ele) => {
+            ele.tags.map((element) => {
+                if (element.startsWith(input)) {
+                    tagsArray.push(ele)
+                    table.innerHTML += `
+                    <tr class="tablerow">
+                        <td id="emojiIcon">${ele.emoji}</td>
+                        <td id="emojiTags">${ele.aliases}</td>
+                        <td id="emojiname">${ele.description}</td>
+                    </tr>
+                    
+                `
+                }
+            })
         })
-    })
-    console.log(tagsArray);
+    }
+    
+    
 }
+
